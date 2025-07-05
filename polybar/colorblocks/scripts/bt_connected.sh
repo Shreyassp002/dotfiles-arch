1d1ceae55965e4532f93d2dafb94ae08858a21b8
Shreyas Patil <shreyassp002@gmail.com>
2025-07-05 09:55:05 +0530

    update

--- polybar/colorblocks/scripts/bt_connected.sh
+++ polybar/colorblocks/scripts/bt_connected.sh
@@ -7,13 +7,16 @@ CONNECTED_COLOR="%{F#f5c4bf}"              # Foreground color for connected (lig
 DISCONNECTED_COLOR="%{F#f8befa}"           # Foreground color for disconnected (light purple)
 RESET="%{F-}"                              # Reset color
 
-# Get the list of connected devices
-connected_device=$(bluetoothctl devices Connected | awk '{$1=$2=""; print $0}' | sed 's/^ *//')
+# Check for connected devices
+connected_device=$(bluetoothctl devices | while read -r type mac name; do
+    if bluetoothctl info "$mac" | grep -q "Connected: yes"; then
+        echo "$name"
+        break
+    fi
+done)
 
 if [ -z "$connected_device" ]; then
-    # Disconnected case: Use red prefix and purple foreground
     echo " ${DISCONNECTED_PREFIX} ${DISCONNECTED_COLOR}Disconnected${RESET} "
 else
-    # Connected case: Use orange prefix and light pink foreground
-    echo " ${CONNECTED_PREFIX} ${CONNECTED_COLOR}$connected_device${RESET} "
+    echo " ${CONNECTED_PREFIX} ${CONNECTED_COLOR}${connected_device}${RESET} "
 fi
